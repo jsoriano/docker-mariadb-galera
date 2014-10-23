aca8dc99d55a9bb34b189c2f0bedda22df24e5a7
Jaime Soriano Pastor <jsoriano@tuenti.com>
2014-10-23 18:41:25 +0200

    It works

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,17 @@ FROM debian:wheezy
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update
-RUN apt-get -y install python-software-properties
-RUN apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-RUN add-apt-repository 'deb http://tedeco.fi.upm.es/mirror/mariadb/repo/10.0/debian wheezy main'
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CBCB082A1BB943DB
+RUN echo 'deb http://mirror.klaus-uwe.me/mariadb/repo/10.0/debian wheezy main' > /etc/apt/sources.list.d/mariadb.list
+RUN apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+RUN echo 'deb http://repo.percona.com/apt wheezy main' > /etc/apt/sources.list.d/percona.list
 RUN apt-get update
 
 RUN apt-get install -y --force-yes mariadb-galera-server galera
+RUN apt-get install -y --force-yes xtrabackup
+
+# Needed for Galera and xtrabackup
+RUN apt-get install -y --force-yes net-tools procps
 
 ADD conf.d /etc/mysql/conf.d
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,52 @@
 #!/bin/bash
 
+MARIADB_CLUSTER_ADDRESS=${MARIADB_CLUSTER_ADDRESS-gcomm://}
+MARIADB_REPLICATION_USER=${MARIADB_REPLICATION_USER-replication}
+MARIADB_SERVER_ID=${MARIADB_SERVER_ID-$(perl -e "print hex('$(hostname | md5sum | cut -c-8)')")}
+MARIADB_SST_METHOD=${MARIADB_SST_METHOD-xtrabackup-v2}
+MARIADB_NODE_ADDRESS=$(head -1 /etc/hosts | awk '{ print $1 }')
+MARIADB_SST_RECEIVE_ADDRESS=${MARIADB_SST_RECEIVE_ADDRESS-$MARIADB_NODE_ADDRESS}
+
+if [ "$MARIADB_CLUSTER_ADDRESS" = "gcomm://" ]; then
+function create_user {
+	local user=$1
+	local password=$2
+	local privileges=$3
+
+	local create_query="CREATE USER '$user'@'%' IDENTIFIED BY '$password'"
+	local grant_query="GRANT $privileges ON *.* TO '$user'@'%' WITH GRANT OPTION"
+	mysql -uroot -e "$create_query;$grant_query;" > /dev/null 2>&1
+}
+
 { 
+	# Wait for mysql to be alive
 	while ! mysql -uroot -e "status" > /dev/null 2>&1;do
 		sleep 1
 	done
 
 	if [ "$MARIADB_PASSWORD" ]; then
-		create_query="CREATE USER 'root'@'%' IDENTIFIED BY PASSWORD '$MARIADB_PASSWORD'"
-		grant_query="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION"
-		mysql -uroot -e "$create_query;$grant_query;"
+		create_user root "$MARIADB_PASSWORD" "ALL PRIVILEGES"
+	fi
+
+	if [ "$MARIADB_REPLICATION_PASSWORD" ]; then
+		create_user "$MARIADB_REPLICATION_USER" "$MARIADB_REPLICATION_PASSWORD" "RELOAD, LOCK TABLES, REPLICATION CLIENT"
 	fi
 } &
+fi
+
 
 cat <<EOF > /etc/mysql/conf.d/galera-node.cnf
 [mysqld]
-wsrep_node_address="$(head -1 /etc/hosts | awk '{ print $1 }')"
+server-id=$MARIADB_SERVER_ID
+wsrep_node_address="$MARIADB_NODE_ADDRESS"
 wsrep_node_name="$(hostname)"
+
+wsrep_cluster_name="$MARIADB_CLUSTER_NAME"
+wsrep_cluster_address="$MARIADB_CLUSTER_ADDRESS"
+
+wsrep_sst_receive_address="$MARIADB_SST_RECEIVE_ADDRESS"
+wsrep_sst_auth=$MARIADB_REPLICATION_USER:$MARIADB_REPLICATION_PASSWORD
+wsrep_sst_method="$MARIADB_SST_METHOD"
 EOF
 
-/usr/bin/mysqld_safe "$@"
+/usr/sbin/mysqld "$@"
